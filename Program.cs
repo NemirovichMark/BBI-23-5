e1181ca93eeffec45bf4f21d2ede400af9466420
Satsik5 <159258169+Satsik5@users.noreply.github.com>
2024-02-07 15:17:26 +0300

    example lab
    ^^

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Random k = new Random();
+double[] a = new double[5];
+double s = 0;
+double c = 0;
+for (int i = 0; i < 5; i++)
+{
+    a[i] = k.Next(10, 50);
+    Console.Write("{0:f0} ", a[i]);
+    s = s + a[i];
+    c = c + 1;
+}
+Console.WriteLine();
+double sred = s / c;
+Console.WriteLine(sred);
+for (int i = 0; i < 5; i++)
+{
+    a[i] = a[i] - sred;
+    Console.Write("{0:f0} ", a[i]);
+}
