f9e18fb4bae536061843f3ff329264376710eaac
Максим <@MAXIM>
2024-02-15 21:57:59 +0300

    6 лаба решение 3х заданий

--- 6 Lab/1 задание.cs
+++ 6 Lab/1 задание.cs
@@ -1,2 +1,96 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public struct Runner
+{
+    public string LastName { get; set; }
+    public string Group { get; set; }
+    public string Teacher { get; set; }
+    public double Result { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Runner> runners = new List<Runner>();
+
+        Console.WriteLine("Введите результаты кросса:");
+
+        while (true)
+        {
+            Console.Write("Фамилия участницы (для прекращения ввода оставьте поле пустым): ");
+            string lastName = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(lastName))
+                break;
+
+            Console.Write("Группа: ");
+            string group = Console.ReadLine();
+
+            Console.Write("Фамилия преподавателя: ");
+            string teacher = Console.ReadLine();
+
+            Console.Write("Результат: ");
+            double result = double.Parse(Console.ReadLine());
+
+            runners.Add(new Runner
+            {
+                LastName = lastName,
+                Group = group,
+                Teacher = teacher,
+                Result = result
+            });
+        }
+
+        Console.Write("Введите норматив: ");
+        double norm = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Результирующая таблица:");
+
+        // Сортировка результатов
+        runners = SortResults(runners);
+
+        // Вывод таблицы
+        Console.WriteLine("{0,-15} {1,-10} {2,-15} {3,-10}", "Фамилия", "Группа", "Преподаватель", "Результат");
+        foreach (Runner runner in runners)
+        {
+            Console.WriteLine("{0,-15} {1,-10} {2,-15} {3,-10}", runner.LastName, runner.Group, runner.Teacher, runner.Result);
+        }
+
+        // Подсчет количества участниц, выполнивших норматив
+        int count = CountParticipantsWithNorm(runners, norm);
+        Console.WriteLine("Количество участниц, выполнивших норматив: " + count);
+    }
+
+    static List<Runner> SortResults(List<Runner> runners)
+    {
+        for (int i = 0; i < runners.Count - 1; i++)
+        {
+            for (int j = 0; j < runners.Count - i - 1; j++)
+            {
+                if (runners[j].Result > runners[j + 1].Result)
+                {
+                    Runner temp = runners[j];
+                    runners[j] = runners[j + 1];
+                    runners[j + 1] = temp;
+                }
+            }
+        }
+
+        return runners;
+    }
+
+    static int CountParticipantsWithNorm(List<Runner> runners, double norm)
+    {
+        int count = 0;
+        foreach (Runner runner in runners)
+        {
+            if (runner.Result <= norm)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
--- 6 Lab/3 задание.cs
+++ 6 Lab/3 задание.cs
@@ -1,2 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Group
+{
+    private int id;
+    public double[] examScores;
+
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    public double[] ExamScores
+    {
+        get { return examScores; }
+        set { examScores = value; }
+    }
+
+    public double CalculateAverageScore()
+    {
+        double sum = 0;
+        for (int i = 0; i < examScores.Length; i++)
+        {
+            sum += examScores[i];
+        }
+        return sum / examScores.Length;
+    }
+}
+
+class Program
+{
+    static void SortGroupsByAverageScore(Group[] groups)
+    {
+        for (int i = 0; i < groups.Length - 1; i++)
+        {
+            for (int j = i + 1; j < groups.Length; j++)
+            {
+                if (groups[i].CalculateAverageScore() < groups[j].CalculateAverageScore())
+                {
+                    Group temp = groups[i];
+                    groups[i] = groups[j];
+                    groups[j] = temp;
+                }
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        const int GroupCount = 3;
+        const int ExamCount = 5;
+
+        Group[] groups = new Group[GroupCount];
+
+        for (int i = 0; i < GroupCount; i++)
+        {
+            Console.WriteLine($"Введите оценки {ExamCount} экзаменов для группы {i + 1}:");
+
+            groups[i] = new Group();
+            groups[i].Id = i + 1;
+            groups[i].ExamScores = new double[ExamCount];
+
+            for (int j = 0; j < ExamCount; j++)
+            {
+                Console.Write($"Оценка {j + 1}: ");
+                groups[i].ExamScores[j] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        SortGroupsByAverageScore(groups);
+
+        Console.WriteLine("Группы в порядке убывания среднего балла:");
+        Console.WriteLine("-------------------------");
+        Console.WriteLine("|    Группа    |  Средний балл  |");
+        Console.WriteLine("-------------------------");
+
+        foreach (Group group in groups)
+        {
+            Console.WriteLine($"|      {group.Id}       |     {group.CalculateAverageScore()}    |");
+        }
+        Console.WriteLine("-------------------------");
+    }
+}
