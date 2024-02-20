2da07d1080b511ad33f3ae5b1ba1ecbe2708ee14
Максим <@MAXIM>
2024-02-20 21:48:29 +0300

    6 lab

---
+++ 6 lab/2 задание.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Text;
+
+struct Student
+{
+    public string Name { get; private set; }
+    private int[] examScores;
+
+    public Student(string name, int score1, int score2, int score3, int score4)
+    {
+        Name = name;
+        examScores = new int[] { score1, score2, score3, score4 };
+    }
+
+    public double CalculateAverage()
+    {
+        return (examScores[0] + examScores[1] + examScores[2] + examScores[3]) / 4.0;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"{Name,-15} {examScores[0],-12} {examScores[1],-12} {examScores[2],-12} {examScores[3],-12}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Student> students = new List<Student>();
+
+        Console.WriteLine("Введите результаты экзаменов для студентов:");
+
+        // Ввод результатов экзаменов для каждого студента
+        while (true)
+        {
+
+
+            Console.Write("Введите имя студента (для прекращения ввода оставьте поле пустым): ");
+
+            string name = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(name))
+                break;
+
+            Console.Write("Оценка за Предмет 1: ");
+            int score1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Оценка за Предмет 2: ");
+            int score2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Оценка за Предмет 3: ");
+            int score3 = int.Parse(Console.ReadLine());
+
+            Console.Write("Оценка за Предмет 4: ");
+            int score4 = int.Parse(Console.ReadLine());
+
+            Student student = new Student(name, score1, score2, score3, score4);
+            students.Add(student);
+        }
+
+
+        int n = students.Count;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+
+                if (students[j].CalculateAverage() < students[j + 1].CalculateAverage())
+                {
+
+                    Student temp = students[j];
+                    students[j] = students[j + 1];
+                    students[j + 1] = temp;
+                }
+            }
+        }
+
+        Console.WriteLine($"{"Name",-15} {"Subject 1",-12} {"Subject 2",-12} {"Subject 3",-12} {"Subject 4",-12}");
+        Console.WriteLine(new string('-', 65));
+
+        foreach (var student in students)
+        {
+            if (student.CalculateAverage() >= 4)
+            {
+                student.DisplayInfo();
+            }
+        }
+    }
+
+}
---
+++ 6 lab/3 задание.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+public struct Student
+{
+    public int Group { get; private set; }
+    private double[] examScores;
+
+    public Student(int group)
+    {
+        Group = group;
+        examScores = new double[4];
+
+        Console.WriteLine($"Введите оценки для группы {Group}:");
+
+        for (int i = 0; i < examScores.Length; i++)
+        {
+            Console.Write($"Оценка {i + 1}: ");
+            examScores[i] = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+
+    public double AverageScore
+    {
+        get
+        {
+            double sum = 0;
+            foreach (var score in examScores)
+            {
+                sum += score;
+            }
+            return sum / examScores.Length;
+        }
+    }
+
+    public void DisplayStudentInfo()
+    {
+        Console.WriteLine($"{Group}\t{AverageScore}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        const int groupSize = 3; // Укажите нужный размер группы
+
+        Student[] students = new Student[groupSize];
+
+        for (int i = 0; i < groupSize; i++)
+        {
+
+
+
+            int group = i + 1;
+
+            students[i] = new Student(group);
+        }
+
+        BubbleSort(ref students);
+
+        Console.WriteLine("\nГруппа\tСредний балл");
+        foreach (var student in students)
+        {
+            student.DisplayStudentInfo();
+        }
+    }
+
+    static void BubbleSort(ref Student[] array)
+    {
+        int n = array.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (array[j].AverageScore < array[j + 1].AverageScore)
+                {
+                    // Обмен элементов массива, если текущий элемент меньше следующего
+                    Student temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
