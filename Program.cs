09e2bff8806147aef8f43d13bbc246dba5a1cd4f
AnnaKolesnikovaBBI <159257945+AnnaKolesnikovaBBI@users.noreply.github.com>
2024-02-26 00:37:17 +0300

    LR6 level 2

--- Program.cs
+++ Program.cs
@@ -1,69 +1,124 @@
-﻿//Радиокомпания провела опрос слушателей по вопросу: «Кого вы считаете человеком года?».
-//Определить пять наиболее часто встречающихся ответов и их долей (в процентах от общего количества ответов). 
+﻿//Соревнования по прыжкам в воду оценивают 7 судей.Каждый спортсмен выполняет 4 прыжка
+//Каждый прыжок имеет одну из шести категорий сложности, оцениваемую коэффициентом (от 2,5 до 3,5)
+//Качество прыжка оценивается судьями по 6-балльной шкале. Далее лучшая и худшая оценки отбрасываются,
+//остальные складываются, и сумма умножается на коэффициент сложности.
+//Получить итоговую таблицу, содержащую фамилии спортсменов, и итоговую оценку (сумму оценок по 4 прыжкам) в порядке занятых мест. 
 using System;
 
-class Programm
+class Diver
 {
-    struct Answer
-    {
-        private string _name;
-        public string Name => _name;
-        private int _count;
-        public int Count => _count;
 
-        public Answer(string name)
+    private string _LastName;
+    public string LastName => _LastName;
+
+
+    private double[] _DiveDifficultyCoefficients;
+    public double[] DiveDifficultyCoefficients => _DiveDifficultyCoefficients;
+
+    private double _FinalScore;
+    public double FinalScore => _FinalScore;
+
+
+    public Diver(string lastName, double[] diveDifficultyCoefficients)
+    {
+        _LastName = lastName;
+        _DiveDifficultyCoefficients = diveDifficultyCoefficients;
+        CalculateTotalScore(); // Вызов метода для расчета итоговой оценки при создании объекта
+    }
+
+    private static void ShellSort(double[] array)
+    {
+        int n = array.Length;
+        int gap = n / 2;
+
+        while (gap > 0)
         {
-            _name = name;
-            _count = 1;
-        }
-        public void ResponsesCounter()
-        {
-            _count++;
+            for (int i = gap; i < n; i++)
+            {
+                double temp = array[i];
+                int j;
+                for (j = i; j >= gap && array[j - gap] < temp; j -= gap)
+                {
+                    array[j] = array[j - gap];
+                }
+                array[j] = temp;
+            }
+            gap /= 2;
         }
     }
-    static void Main()
+
+    // Вот этот метод для вычисления итоговой оценки спортсмена
+    private void CalculateTotalScore()
     {
-        Answer[] answ = new Answer[] { }; //массив, через добавление в который будем считать ответы
-        int PersentCounter = 0;
-        string[] Responses = new[]{ "Эмма Уотсон", "Анастатися Ивлеева", //все ответы
-            "Джон Смит", "Константин Ивлев", "Константин Ивлев",
-            "Константин Ивлев","Эмма Уотсон","Эмма Уотсон","Эмма Уотсон",
-            "Эмма Уотсон","Джон Смит","Мария Иванова","Анастатися Ивлеева",
-            "Константин Ивлев","Александр Петров","Мария Иванова",
-            "Эмма Уотсон","Анастатися Ивлеева","Джон Смит","Мария Иванова",
-            "Эмма Уотсон","Константин Ивлев","Анастатися Ивлеева"};
-        for (int i = 0; i < Responses.Length; i++)
+        double totalScore = 0;
+        Random rnd = new Random();
+
+        for (int j = 0; j < DiveDifficultyCoefficients.Length; j++)
         {
-            string[] names = answ.Select(x => x.Name).ToArray();
-            if (names.Contains(Responses[i]))
-                answ[Array.IndexOf(names, Responses[i])].ResponsesCounter();
-            else
-                answ = answ.Append(new Answer(Responses[i])).ToArray();
-            PersentCounter++;
+            double[] judgeScores = new double[7];
+            for (int i = 0; i < 7; i++)
+            {
+                judgeScores[i] = rnd.NextDouble() * 5.0 + 1.0; //оценки судей я не вводила, они просто сгенерируются от 1 до 6
+            }
+            ShellSort(judgeScores);
+            double jumpScore = 0;
+            for (int i = 1; i < 6; i++)
+            {
+                jumpScore += judgeScores[i];
+            }
+
+            totalScore += jumpScore * DiveDifficultyCoefficients[j];
         }
 
-        static void SelectionSort(Answer[] Array)
+        _FinalScore = Math.Round(totalScore, 2); 
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        //сортировка выбором
+        static void SelectionSort(Diver[] Array)
         {
             for (int i = 0; i < Array.Length - 1; i++)
             {
                 int min = i;
                 for (int j = i + 1; j < Array.Length; j++)
                 {
-                    if (Array[j].Count > Array[min].Count)
+                    if (Array[j].FinalScore > Array[min].FinalScore)
                     {
                         min = j;
                     }
                 }
-                Answer dummy = Array[i];
+                Diver d = Array[i];
                 Array[i] = Array[min];
-                Array[min] = dummy;
+                Array[min] = d;
                 min = i;
             }
         }
-        SelectionSort(answ);
-        for (int i = 0; i < 5; i++)
+
+        Diver[] divers = new Diver[]
         {
-            Console.WriteLine($"На {i + 1} месте: {answ[i].Name} - {answ[i].Count * 100 / PersentCounter}%");
+            new Diver("Иванов",new double []{2.5, 2.7, 2.5, 3.0}),
+            new Diver("Петров",new double []{3.4, 2.7, 3.2, 3.5}),
+            new Diver("Ткачук",new double []{2.5, 2.5, 3.2, 3.2}),
+            new Diver("Фролов",new double []{2.5, 2.7, 2.5, 3.0}),
+            new Diver("Семенов",new double []{2.5, 2.7, 3.5, 3.2}),
+            new Diver("Белых",new double []{3.4, 3.2, 3.2, 3.5}),
+            new Diver("Жучков",new double []{2.5, 2.5, 2.5, 3.0}),
+            new Diver("Громких",new double []{3.4, 3.0, 3.0, 3.5}),
+            new Diver("Игнатьев",new double []{2.5, 2.7, 3.2, 3.2})
+        };
+        SelectionSort(divers);
+
+
+        Console.WriteLine("Место\tФамилия спортсмена\tИтоговая оценка");
+        int place = 1;
+        foreach (var diver in divers)
+        {
+            Console.WriteLine($"{place}\t{diver.LastName}\t\t\t{diver.FinalScore}");
+            place++;
         }
     }
 }
