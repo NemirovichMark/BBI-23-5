1b375915d353ad379feca2b033365005dcad48a1
Andreeva30 <159258130+Andreeva30@users.noreply.github.com>
2024-02-27 21:43:43 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,88 +1,48 @@
-﻿////3 уровень 4 задание 
-//using System;
+﻿//1 уровень 4 задание 
+using System;
 
+namespace HighJumpCompetition
+{
+    class Program
+    {
+        class Participant
+        {
+            public string Name { get; set; }
+            public double BestAttempt { get; set; }
+        }
 
-//struct RaceParticipant
-//{
-//    public string lastName;
-//    public float result;
-//}
+        static void BubbleSort(Participant[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j].BestAttempt < arr[j + 1].BestAttempt)
+                    {
+                        Participant temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
 
-//class Program
-//{
-//    static void Main(string[] args)
-//    {
-        
-//        RaceParticipant[] group1 = new RaceParticipant[5];
-//        RaceParticipant[] group2 = new RaceParticipant[5];
+        static void Main(string[] args)
+        {
+            Participant[] participants = new Participant[5];
+            participants[0] = new Participant { Name = "Anna", BestAttempt = 1.75 };
+            participants[1] = new Participant { Name = "Bob", BestAttempt = 1.80 };
+            participants[2] = new Participant { Name = "Sveta", BestAttempt = 1.70 };
+            participants[3] = new Participant { Name = "David", BestAttempt = 1.85 };
+            participants[4] = new Participant { Name = "Eva", BestAttempt = 1.90 };
 
-        
-//        group1[0].lastName = "Иванов";
-//        group1[0].result = 15.2f;
+            BubbleSort(participants);
 
-//        group1[1].lastName = "Петров";
-//        group1[1].result = 15.5f;
-
-//        group1[2].lastName = "Сидоров";
-//        group1[2].result = 15.7f;
-
-//        group1[3].lastName = "Козлов";
-//        group1[3].result = 15.3f;
-
-//        group1[4].lastName = "Смирнов";
-//        group1[4].result = 15.1f;
-
-        
-//        group2[0].lastName = "Алексеев";
-//        group2[0].result = 14.8f;
-
-//        group2[1].lastName = "Борисов";
-//        group2[1].result = 14.9f;
-
-//        group2[2].lastName = "Григорьев";
-//        group2[2].result = 14.7f;
-
-//        group2[3].lastName = "Дмитриев";
-//        group2[3].result = 14.6f;
-
-//        group2[4].lastName = "Егоров";
-//        group2[4].result = 14.5f;
-
-        
-//        RaceParticipant[] allParticipants = new RaceParticipant[group1.Length + group2.Length];
-
-//        for (int i = 0; i < group1.Length; i++)
-//        {
-//            allParticipants[i] = group1[i];
-//        }
-
-//        for (int i = 0; i < group2.Length; i++)
-//        {
-//            allParticipants[group1.Length + i] = group2[i];
-//        }
-
-        
-//        for (int i = 0; i < allParticipants.Length - 1; i++)
-//        {
-//            for (int j = i + 1; j < allParticipants.Length; j++)
-//            {
-//                if (allParticipants[i].result > allParticipants[j].result)
-//                {
-//                    RaceParticipant temp = allParticipants[i];
-//                    allParticipants[i] = allParticipants[j];
-//                    allParticipants[j] = temp;
-//                }
-//            }
-//        }
-
-        
-//        Console.WriteLine("Участник\t\tРезультат");
-//        Console.WriteLine("---------------------------------");
-
-//        foreach (var participant in allParticipants)
-//        {
-//            Console.WriteLine($"{participant.lastName,-10}\t\t{participant.result}");
-//        }
-
-//    }
-//}
+            for (int i = 0; i < participants.Length; i++)
+            {
+                Console.WriteLine("{0}. {1} - {2} м", i + 1, participants[i].Name, participants[i].BestAttempt);
+            }
+        }
+    }
+}
