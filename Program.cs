ceeee3bfdda5691d86d279fbf5465ba244694c09
MarininOleg <159258512+MarininOleg@users.noreply.github.com>
2024-02-28 17:36:49 +0300

    измененная 3 задача

--- Program.cs
+++ Program.cs
@@ -1,54 +1,80 @@
-﻿using System;
-class Programm
-{ 
-    struct Uchastnik
+﻿
+class Program
+{        
+    struct Lizhniki
     {
-        private string lastname, obzhestvo;
-        private double result1, result2, summ;
-        public Uchastnik(string ln, string obzh, double r1, double r2)
+        private string lastname;
+        private int group;
+        public double deltatime;
+
+        public Lizhniki(string ln, int gr, double dt)
         {
             lastname = ln;
-            obzhestvo = obzh;
-            result1 = r1;
-            result2 = r2;
-            summ = r1 + r2;
+            group = gr;
+            deltatime = dt;
         }
-        static void PrintUch(Uchastnik[] uchastniks)
-        {
-            Console.WriteLine("Фамилия    Общество   FA    SA    Сумма"); //FA-first attempt, SA-second attempt.
-            for (int i = 0; i < uchastniks.Length; i++)
-            {
-                Console.WriteLine($"{uchastniks[i].lastname}    {uchastniks[i].obzhestvo}  {uchastniks[i].result1}    {uchastniks[i].result2,-2}    {uchastniks[i].summ}");
-            }
-        }
-        static void Main()
-        {
-            Uchastnik[] uchastniks = {
-        new Uchastnik("Петров","общество1",8.1,7.3),
-        new Uchastnik("Иванов","общество2",7.5, 7.1),
-        new Uchastnik("Сидоров","общество3",6.7,6.3),
-        new Uchastnik("Крутов", "общество4",7.7,7.8),
-        new Uchastnik("Молодов","общество5",7.2,6.9),
-        };
-            sortirovka(uchastniks);
-            PrintUch(uchastniks);
-        }
-        static void sortirovka(Uchastnik[] uchastniks)
-        {
-            for (int i = 0; i < uchastniks.Length; i++)
-            {
-                for (int j = 0; j < uchastniks.Length - i - 1; j++)
-                {
-                    if (uchastniks[j].summ < uchastniks[j + 1].summ)
-                    {
-                        Uchastnik t = uchastniks[j];
-                        uchastniks[j] = uchastniks[j + 1];
-                        uchastniks[j + 1] = t;
-                    }
-                }
-            }
 
+        public void Print(int place)
+        {
+            Console.WriteLine(" {0, 3}  {1, 3}  {2, 10}  {3}m {4, 2:f0}s ", place, group, lastname, Math.Floor(deltatime / 60), 60 * (deltatime / 60 - Math.Floor(deltatime / 60)));
         }
+
     }
+    static void Main(string[] args)
+    {
 
+
+        int N = 10;
+        Random rand = new Random();
+        string[] lastnames =
+        {
+            "Бе",
+            "Большунов",
+            "Шипулин",
+            "Иванов",
+            "Кленов",
+            "Джака",
+            "Володцев",
+            "Ярмолин",
+            "Дамирбаев",
+        };
+
+        Lizhniki[] results = new Lizhniki[N];
+        for (int i = 0; i < N; i++)
+        {
+            int lastnamesIndex = rand.Next(lastnames.Length);
+            double result = 120 + rand.NextDouble() *100;
+            Lizhniki snowData = new Lizhniki(lastnames[lastnamesIndex], i % 2, result);
+            results[i] = snowData;
+        }
+        static void Sort(Lizhniki[] results, int n)
+        {
+            int i = 0;
+            while (i < n)
+            {
+                if (i == 0)
+                    i++;
+                if (results[i].deltatime >= results[i - 1].deltatime)
+                    i++;
+                else
+                {
+                    Lizhniki t = results[i];
+                    results[i] = results[i - 1];
+                    results[i - 1] = t;
+                    i--;
+                }
+
+
+            }
+        }
+        Sort(results, N);
+        Console.WriteLine("Место  Группа Фамилия  Время");
+        
+        int place = 1;
+        foreach (Lizhniki data in results)
+        {
+            data.Print(place++);
+        }
+
+    }
 }
