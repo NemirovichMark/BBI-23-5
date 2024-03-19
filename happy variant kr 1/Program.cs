3e6b45c429b88179e69657a2a5bac289be870542
Satsik5 <159258169+Satsik5@users.noreply.github.com>
2024-03-19 21:12:51 +0300

    5 в з 1

--- happy variant kr 1/Program.cs
+++ happy variant kr 1/Program.cs
@@ -1,57 +1,89 @@
-﻿struct Goods
+﻿struct Book
 {
     private string _name;
-    private string _artic;
-    private int _cost;
-    public int Cost => _cost;
-    private int _kolvo;
-    public Goods(string name, string artic, int cost, int kolvo)
+    private int _ISBN;
+    private string _author;
+    private int _year;
+    public int Year => _year;
+    public string Author => _author;
+    public Book(string name, int ISBN, string author, int year)
     {
         _name = name;
-        _artic = artic;
-        _cost = cost;
-        _kolvo = kolvo;
+        _ISBN = ISBN;
+        _author = author;
+        _year = year;
     }
     public void Print()
     {
-        Console.WriteLine("Товар   {0}\t Количество {1}\t Информация {2}\t",
-                        _name, _kolvo, _artic);
+        Console.WriteLine("Автор   {0}\t Книга {1}\t ISBN {2}\t Год издания {3}\t",
+                        _author, _name, _ISBN, _year);
     }
 }
 class Program
 {
     static void Main()
     {
-        Goods[] c1 = new Goods[]
+        Book[] books = new Book[]
         {
-            new Goods("мыло","хозяйственное",100,5),
-            new Goods("вода","минеральная",25,100),
-            new Goods("каша","перловая",38,145),
-            new Goods("крем","для рук",300,4),
-            new Goods("лампа","настольная",1500,10)
+            new Book("Война и мир",12546, "Толстой", 1889),
+            new Book("Геометрия",54782, "Мерзляк", 2015),
+            new Book("Айболит",32456, "Чуковский", 1984),
+            new Book("Пишисокращай",43567, "Иванов", 2019),
+            new Book("1984",21332, "Оруэлл", 1949),
+            new Book("Обломов",13546, "Гончаров", 1931),
+            new Book("Физика",12346, "Перышкин", 2000),
+            new Book("Золотой луг",25346, "Пришвин", 1934),
+            new Book("Гарри Поттер",56314, "Роулинг", 1997),
+            new Book("Три товарища",44546, "Ремарк", 1936)
+
         };
-        Sorting(c1);
-        Console.WriteLine();
-        foreach (Goods good in c1)
+        foreach (var book in books)
         {
-            good.Print();
+            book.Print();
         }
-        static void Sorting(Goods[] c1)
+        Author(books);
+        Century(books);
+
+    }
+    static void Author(Book[] books)
+    {
+        Console.WriteLine("Введите фамилию автора для поиска книг: ");
+        string AuthorInput = "Пришвин";
+        Console.WriteLine("Книги автора {0}:", AuthorInput);
+        bool found = false;
+        foreach (var book in books)
         {
-            int n = c1.Length;
-            for (int i = 0; i < n - 1; i++)
+            if (book.Author == AuthorInput)
             {
-                for (int j = 0; j < n - i - 1; j++)
-                {
-                    if (c1[j].Cost > c1[j + 1].Cost)
-                    {
-                        Goods temp = c1[j];
-                        c1[j] = c1[j + 1];
-                        c1[j + 1] = temp;
-                    }
-                }
+                book.Print();
+                found = true;
             }
         }
+        if (!found)
+        {
+            Console.WriteLine("Книг этого автора не найдено");
+        }
+    }
+    static void Century(Book[] books)
+    {
+        Console.WriteLine("Введите век для поиска книг: ");
+        int centuryInput = 21;
+        int startYear = centuryInput * 100 - 100;
+        int endYear = centuryInput * 100 - 1;
 
-    }  
+        Console.WriteLine("Книги, написанные в {0} веке:", centuryInput);
+        bool found = false;
+        foreach (var book in books)
+        {
+            if (book.Year >= startYear && book.Year <= endYear)
+            {
+                book.Print();
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine("Книг этого века не найдено");
+        }
+    }
 }
