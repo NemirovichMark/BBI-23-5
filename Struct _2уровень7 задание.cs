4f687d2ced6977794638fd793d9b90ae90cdfeb4
Chufarovskaya <159258450+Chufarovskaya@users.noreply.github.com>
2024-03-19 22:23:51 +0300

    Add files via upload

---
+++ Struct _2уровень7 задание.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика_5_2_семестр
+{
+    internal struct Chess
+    {
+        public string Surname {  get; private set; }
+        public double Game1 { get; private set; }
+        public double Game2 { get; private set;}
+        public double Game3 { get; private set;}
+
+        public Chess (string surname, double game1, double game2, double game3)
+        {
+            Surname = surname;
+            Game1 = game1;
+            Game2 = game2;
+            Game3 = game3;
+        }
+
+
+    }
+    internal struct Result
+    {
+        public string Surname { get; private set; }
+        public double Game123 { get; private set;}
+
+        public Result(string aurname, double game123)
+        {
+            Surname= aurname;
+            Game123 = game123;
+        }
+    }
+
+}
