cb0bbd4a447d1aa027744f8b44a7a0813125c299
AnnaKolesnikovaBBI <159257945+AnnaKolesnikovaBBI@users.noreply.github.com>
2024-03-29 21:19:30 +0300

    LR7_level2

--- Program.cs
+++ Program.cs
@@ -1,289 +1,200 @@
-﻿using System;
-
-//Создать класс «Группа» с набором общих для всех групп-наследников предметов (3-4) и от него 3 наследника (названия групп)
-//с дополнительными 2 различными предметами. В наследниках переопределить метод расчета среднего балла за сессию.
-//Вывести студентов всех групп единым списком. Использовать динамическую связку: преобразование классов.
-
-
-//Абстрактный класс группы
-public abstract class Group
+﻿//Сделать абстрактный класс «Прыжки в воду» с обязательным полем «Название дисциплины», и от него наследников: «с 3м» и «с 5м».
+//В каждом из классов переопределить название дисциплины (и выводить в начале таблицы).
+public class Diver
 {
-    protected string _name { get; set; }
-    protected double _averagemark;
-    public double AverageMark;
-    public Subject[] Subjects { get; set; }
-    public Student[] Students { get; set; }
+    private string _lastname;
+    public string LastName => _lastname;
 
-    //метод для наполнения базовых предметов
-    public void SetMainSubjects()
+    public Jump[] Jumps;
+
+    public double TotalScore;
+
+    public Diver(string lastName, Jump[] jumps)
     {
-        Subjects = new Subject[5]
-        {
-            new Subject("Русский язык"),
-            new Subject("Математика"),
-            new Subject("Физика"),
-            new Subject("Английский язык"),
-            new Subject("История")
-        };
+        _lastname = lastName;
+        Jumps = jumps;
+
+        //Сразу же делаем так, что бы спортсмен при создании прыгал несколько раз
+        foreach (var jump in Jumps)
+            jump.Evaluate();
+
+        CalculateTotalScore();
     }
 
-    public abstract void CalibrateSubjects();
-
-    public abstract void ShowReport();
-}
-
-public class GroupA : Group
-{
-    //Доп. предметы
-    private Subject AdditionalSubjectAA { get; set; }
-    private Subject AdditionalSubjectAB { get; set; }
-
-    public GroupA(string name, Student[] students)
+    public double CalculateTotalScore()
     {
-        _name = name;
-        Students = students;
-        SetMainSubjects();
-        AdditionalSubjectAA = new Subject("Химия");
-        AdditionalSubjectAB = new Subject("Органическая химия");
-    }
-    //Вычисление среднего балла
-    public override void CalibrateSubjects()
-    {
-        double summOfScore = 0;
+        foreach (var jump in Jumps)
+            TotalScore += jump.TotalScore;
 
-        foreach (var subject in Subjects)
-            summOfScore += subject.Score;
+        TotalScore = Math.Round(TotalScore, 2);
 
-        summOfScore += AdditionalSubjectAA.Score;
-        summOfScore += AdditionalSubjectAB.Score;
-
-        _averagemark = summOfScore / (Subjects.Length + 2);
-    }
-
-    public override void ShowReport()
-    {
-        Console.WriteLine($"====== Группа {_name} ======");
-        Console.WriteLine($"Средний балл: {_averagemark}");
-
-        foreach (var subject in Subjects)
-        {
-            Console.WriteLine($"{subject.Name} Оценка: {subject.Score}");
-        }
-
-        Console.WriteLine($"{AdditionalSubjectAA.Name} Оценка: {AdditionalSubjectAA.Score}");
-        Console.WriteLine($"{AdditionalSubjectAB.Name} Оценка: {AdditionalSubjectAB.Score}");
-
-
-        Console.WriteLine($"===========================");
+        return TotalScore;
     }
 }
-public class GroupB : Group
+public class Jump
 {
-    //Доп. предметы
-    private Subject AdditionalSubjectBA { get; set; }
-    private Subject AdditionalSubjectBB { get; set; }
+    public double Rate;
 
-    public GroupB(string name, Student[] students)
+    public double[] Scores = new double[7];
+
+    public double TotalScore;
+
+    //Конструктор, который создаёт прыжок и автоматически назначает ему коэф.
+    public Jump()
     {
-        _name = name;
-        Students = students;
-        SetMainSubjects();
-        AdditionalSubjectBA = new Subject("Экономика");
-        AdditionalSubjectBB = new Subject("Маркетинг");
-    }
-    //Вычисление среднего балла
-    public override void CalibrateSubjects()
-    {
-        double summOfScore = 0;
-
-        foreach (var subject in Subjects)
-            summOfScore += subject.Score;
-
-        summOfScore += AdditionalSubjectBA.Score;
-        summOfScore += AdditionalSubjectBB.Score;
-
-        AverageMark = summOfScore / (Subjects.Length + 2);
+        var random = new Random().Next(25, 36);
+        Rate = (double)random / (double)10;
     }
 
-    public override void ShowReport()
+    //Конструктор, который создаёт прыжок и куда подаётся коэф.
+    public Jump(double rate)
     {
-        Console.WriteLine($"====== Группа {_name} ======");
-        Console.WriteLine($"Средний балл: {AverageMark}");
+        Rate = rate;
+    }
 
-        foreach (var subject in Subjects)
+    //Результаты прыжка
+    public void Evaluate()
+    {
+        for (int i = 0; i < 7; i++)
         {
-            Console.WriteLine($"{subject.Name} Оценка: {subject.Score}");
+            Scores[i] = new Random().Next(100, 601) / 100;
         }
 
-        Console.WriteLine($"{AdditionalSubjectBA.Name} Оценка: {AdditionalSubjectBA.Score}");
-        Console.WriteLine($"{AdditionalSubjectBB.Name} Оценка: {AdditionalSubjectBB.Score}");
 
+        _shellSort(Scores);
 
-        Console.WriteLine($"===========================");
-    }
-}
+        //Обнуление самой лучшей и худшей оценки
+        Scores[0] = 0;
+        Scores[6] = 0;
 
-public class GroupC : Group
-{
-    //Доп. предметы
-    private Subject AdditionalSubjectCA { get; set; }
-    private Subject AdditionalSubjectCB { get; set; }
+        double tempScore = 0;
 
-    public GroupC(string name, Student[] students)
-    {
-        _name = name;
-        Students = students;
-        SetMainSubjects();
-        AdditionalSubjectCA = new Subject("Дискретная математика");
-        AdditionalSubjectCB = new Subject("Алгоритмизация");
-    }
-    //вычисление среднего балла
-    public override void CalibrateSubjects()
-    {
-        double summOfScore = 0;
-
-        foreach (var subject in Subjects)
-            summOfScore += subject.Score;
-
-        summOfScore += AdditionalSubjectCA.Score;
-        summOfScore += AdditionalSubjectCB.Score;
-
-        AverageMark = summOfScore / (Subjects.Length + 2);
-    }
-    //Отображение отчёта по группе
-    public override void ShowReport()
-    {
-        Console.WriteLine($"====== Группа {_name} ======");
-        Console.WriteLine($"Средний балл: {AverageMark}");
-
-        foreach (var subject in Subjects)
+        foreach (double score in Scores)
         {
-            Console.WriteLine($"{subject.Name} Оценка: {subject.Score}");
+            tempScore += score;
         }
 
-        Console.WriteLine($"{AdditionalSubjectCA.Name} Оценка: {AdditionalSubjectCA.Score}");
-        Console.WriteLine($"{AdditionalSubjectCB.Name} Оценка: {AdditionalSubjectCB.Score}");
+        TotalScore = tempScore * Rate;
 
-
-        Console.WriteLine($"===========================");
-    }
-}
-
-//Поток
-public class Stream
-{
-    public Group[] Groupes;
-
-    public Stream(Group[] groupes)
-    {
-        Groupes = groupes;
     }
 
-    //Метод, который нужен для отображения отчёта
-    public void ShowReport()
+    private static void _shellSort(double[] array)
     {
-        foreach (var groupe in Groupes)
-        {
-            groupe.CalibrateSubjects();
-        }
+        int n = array.Length;
+        int gap = n / 2;
 
-        _selectionSort();
-
-        foreach (var groupe in Groupes)
+        while (gap > 0)
         {
-            groupe.ShowReport();
-        }
-
-        Console.WriteLine("===== Студенты потока =====");
-        foreach (var groupe in Groupes)
-        {
-            foreach (var student in groupe.Students)
+            for (int i = gap; i < n; i++)
             {
-                Console.WriteLine(student.Name + " " + student.Surname);
+                double temp = array[i];
+                int j;
+                for (j = i; j >= gap && array[j - gap] < temp; j -= gap)
+                {
+                    array[j] = array[j - gap];
+                }
+                array[j] = temp;
             }
+            gap /= 2;
         }
+    }
+}
+public abstract class WaterJumps
+{
+    public abstract string DisciplineName { get; set; }
 
-        Console.WriteLine("===========================");
+    public Diver[] Divers;
+
+    public void Start()
+    {
+        _selectionSort(Divers);
+
+        Console.WriteLine("Тип: " + DisciplineName);
+        Console.WriteLine("Место\tФамилия спортсмена\tИтоговая оценка");
+        int place = 1;
+        foreach (var diver in Divers)
+        {
+            Console.WriteLine($"{place}\t{diver.LastName}\t\t\t{diver.TotalScore}");
+            place++;
+        }
     }
 
-    private void _selectionSort()
+
+    private void _selectionSort(Diver[] Array)
     {
-        for (int i = 0; i < Groupes.Length - 1; i++)
+        for (int i = 0; i < Array.Length - 1; i++)
         {
             int min = i;
-            for (int j = i + 1; j < Groupes.Length; j++)
+            for (int j = i + 1; j < Array.Length; j++)
             {
-                if (Groupes[j].AverageMark > Groupes[min].AverageMark)
+                if (Array[j].TotalScore > Array[min].TotalScore)
                 {
                     min = j;
                 }
             }
-            Group m = Groupes[i];
-            Groupes[i] = Groupes[min];
-            Groupes[min] = m;
+            Diver d = Array[i];
+            Array[i] = Array[min];
+            Array[min] = d;
             min = i;
         }
     }
 }
-public class Student
+internal class WaterJumps3m : WaterJumps
 {
-    public string Name { get; set; }
-    public string Surname { get; set; }
+    public override string DisciplineName { get; set; }
 
-    public Student(string name, string surname)
+    public WaterJumps3m(Diver[] divers)
     {
-        Name = name;
-        Surname = surname;
+        DisciplineName = "Прыжки 3 метра";
+        Divers = divers;
     }
 }
-//Предмет
-public class Subject
+internal class WaterJumps5m : WaterJumps
 {
-    public string Name { get; set; }
-    public int Score { get; set; }
+    public override string DisciplineName { get; set; }
 
-    public Subject(string name)
+    public WaterJumps5m(Diver[] divers)
     {
-        Name = name;
-        Score = new Random().Next(1, 6);
-    }
-    public Subject(string name, int score)
-    {
-        Name = name;
-        Score = score;
+        DisciplineName = "Прыжки 5 метров";
+        Divers = divers;
     }
 }
 class Program
 {
     static void Main()
     {
-        var Groupes = new Group[]
+        Diver[] divers = new Diver[]
         {
-            new GroupA("Группа А", new Student[]
-            {
-                new Student("Виктория", "Ульянова"),
-                new Student("Марк", "Богатырёв"),
-                new Student("Олег", "Романов")
-            }),
-            new GroupB("Группа В", new Student[]
-            {
-                new Student("Мария", "Илонова"),
-                new Student("Глеб", "Останкин"),
-                new Student("Павел", "Васильев")
-            }),
-            new GroupC("Группа С", new Student[]
-            {
-                new Student("Анастасия", "Милова"),
-                new Student("Марк", "Богатырёв"),
-                new Student("Нина", "Светова")
-            })
+            new Diver("Иванов", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()}),
+            new Diver("Петров", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()}),
+            new Diver("Ткачук", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()}),
+            new Diver("Фролов", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()}),
+            new Diver("Семенов", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()}),
+            new Diver("Белых", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()}),
+            new Diver("Жучков", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()}),
+            new Diver("Громких", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()}),
+            new Diver("Игнатьев", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()})
         };
 
-        var Stream = new Stream(Groupes);
-        Stream.ShowReport();
+        //Демонстрация работы класса 3 метровых прыжков
+        WaterJumps WaterJumps3m = new WaterJumps3m(divers);
+        WaterJumps3m.Start();
 
+        Diver[] divers2 = new Diver[]
+{
+            new Diver("Иванов", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()}),
+            new Diver("Петров", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()}),
+            new Diver("Ткачук", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()}),
+            new Diver("Фролов", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()}),
+            new Diver("Семенов", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()}),
+            new Diver("Белых", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()}),
+            new Diver("Жучков", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()}),
+            new Diver("Громких", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()}),
+            new Diver("Игнатьев", new Jump[]{new Jump(), new Jump(), new Jump(), new Jump()})
+};
 
+        //Демонстрация работы класса 5 метровых прыжков
+        WaterJumps WaterJumps5m = new WaterJumps5m(divers2);
+        WaterJumps5m.Start();
     }
 }
-
-
