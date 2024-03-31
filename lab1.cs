7ede959584d2409435a3930185a346f33e2ecc3c
Balashova04 <160737068+Balashova04@users.noreply.github.com>
2024-03-31 23:53:26 +0300

    Add files via upload

--- lab1.cs
+++ lab1.cs
@@ -1,63 +1,1 @@
-/* Результаты соревнований по прыжкам в высоту определяются по лучшей из двух
-попыток. Вывести список участников в порядке занятых мест.*/
-using System;
-
-namespace lab1
-{
-    class Sportsmen
-    {
-        public string FIO { get; private set; }
-        public double Res1 { get; private set; }
-        public double Res2 { get; private set; }
-        public double Res { get; private set; }
-
-        public Sportsmen(string FIO, double Res1, double Res2)
-        {
-            this.FIO = FIO;
-            this.Res1 = Res1;
-            this.Res2 = Res2;
-            this.Res = Math.Max(Res1, Res2);
-        }
-    }
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
-            Sportsmen[] Players = new Sportsmen[4];
-            Players[0] = new Sportsmen("Иванов      Иван        Иванович    ", 1.50, 1.53);
-            Players[1] = new Sportsmen("Пупкин      Василий     Васильевич  ", 1.47, 1.36);
-            Players[2] = new Sportsmen("Пушкин      Александр   Сергеевич   ", 1.52, 1.45);
-            Players[3] = new Sportsmen("Лермонтов   Михаил      Юрьевич     ", 1.49, 1.61);
-
-            for (int i = 0; i < Players.Length; i++)
-            {
-                Console.WriteLine(Players[i].FIO + " " + Players[i].Res);
-            }
-            Console.WriteLine();
-
-            for (int i = 0; i < Players.Length - 1; i++)
-            {
-                double amax = Players[i].Res;
-                int imax = i;
-                for (int j = i + 1; j < Players.Length; j++)
-                {
-                    if (Players[j].Res > amax)
-                    {
-                        amax = Players[j].Res;
-                        imax = j;
-                    }
-                }
-                Sportsmen temp;
-                temp = Players[imax];
-                Players[imax] = Players[i];
-                Players[i] = temp;
-            }
-
-            for (int i = 0; i < Players.Length; i++)
-            {
-                Console.WriteLine(Players[i].FIO + " " + Players[i].Res);
-            }
-
-        }
-    }
-}
+/* Результаты соревнований по прыжкам в высоту определяются по лучшей из двухпопыток. Вывести список участников в порядке занятых мест.*/using System;namespace lab1{    class Sportsmen    {        public string FIO { get; private set; }        public double Res1 { get; private set; }        public double Res2 { get; private set; }        public bool Disc { get; private set; }        public double Res { get; private set; }        public Sportsmen(string FIO, double Res1, double Res2)        {            this.FIO = FIO;            this.Res1 = Res1;            this.Res2 = Res2;            this.Disc = False;            this.Res = Math.Max(Res1, Res2);        }        public static void Disqualification()        {            this.Disc = True;        }    }    class MainClass    {        public static void Main(string[] args)        {            Sportsmen[] Players = new Sportsmen[4];            Players[0] = new Sportsmen("Иванов      Иван        Иванович    ", 1.50, 1.53, True);            Players[1] = new Sportsmen("Пупкин      Василий     Васильевич  ", 1.47, 1.36, False);            Players[2] = new Sportsmen("Пушкин      Александр   Сергеевич   ", 1.52, 1.45, False);            Players[3] = new Sportsmen("Лермонтов   Михаил      Юрьевич     ", 1.49, 1.61), False);            Players[0].Disqualification()            for (int i = 0; i < Players.Length; i++)            {                Console.WriteLine(Players[i].FIO + " " + Players[i].Res);            }            Console.WriteLine();            for (int i = 0; i < Players.Length - 1; i++)            {                double amax = Players[i].Res;                int imax = i;                for (int j = i + 1; j < Players.Length; j++)                {                    if (Players[j].Res > amax)                    {                        amax = Players[j].Res;                        imax = j;                    }                }                Sportsmen temp;                temp = Players[imax];                Players[imax] = Players[i];                Players[i] = temp;            }            for (int i = 0; i < Players.Length; i++)            {                if (!Players[i].Disc)                {                    Console.WriteLine(Players[i].FIO + " " + Players[i].Res);                }            }        }    }}
