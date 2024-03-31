f22bba186d72d821e27d2f358b5a53dfc98b6bfd
alisaselez <159687758+alisaselez@users.noreply.github.com>
2024-03-31 17:05:36 +0300

    7 lab

---
+++ 7 lab lv 2 - num 3 2.0.cs
@@ -0,0 +1,98 @@
+﻿//lv2 num 3 лаба 7
+
+using System;
+
+public class  Sportsmens
+{
+    private string _lastname;
+    private double result1;
+    private double result2;
+    private double result3;
+    public Sportsmens(string lastname, double res1, double res2, double res3)
+    {
+        _lastname = lastname;
+        result1 = res1;
+        result2 = res2;
+        result3 = res3;
+    }
+    public string Lastname()
+    {
+        return _lastname;
+    }
+
+    public double Bestresult()
+    {
+        return Math.Max(result1, Math.Max(result2, result3));
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Sportsmens: {Lastname()}");
+        Console.WriteLine($"Best result: {Bestresult()}");
+        Console.WriteLine();
+    }
+}
+public abstract class Discipline
+{
+    public abstract string DisciplineName { get; }
+    private Sportsmens _sportsmens;
+    
+    public Discipline(Sportsmens sportsmens)
+    {
+        _sportsmens = sportsmens;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Discipline: {DisciplineName}");
+        _sportsmens.Print();
+        Console.WriteLine();
+    }
+}
+public class LongJump : Discipline
+{
+    public override string DisciplineName => "прыжки в длину";
+    public LongJump(Sportsmens sportsmens) : base(sportsmens)
+    {
+
+    }
+
+}
+public class HightJump : Discipline
+{
+    public override string DisciplineName => "прыжки в высоту";
+    public HightJump(Sportsmens sportsmens) : base(sportsmens)
+    {
+
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        Sportsmens[] sportsmens =
+        {
+            new Sportsmens("Евелькин", 4.9, 5.9, 6.0),
+            new Sportsmens("Smith", 5.6, 5.8, 5.7),
+            new Sportsmens("Johnson", 5.5, 5.9, 6.1),
+            new Sportsmens("Носыч", 4.8, 7.1, 7.0),
+            new Sportsmens("Мбапе", 4.9,5.9,6.1)
+        };
+        Discipline[] discipline =
+        {
+            new HightJump(sportsmens[0]),
+            new LongJump(sportsmens[1]),
+            new LongJump(sportsmens[2]),
+            new LongJump(sportsmens[3]),
+            new HightJump(sportsmens[4])
+        };
+
+        Console.WriteLine("Final Results:");
+        for (int i = 0; i < discipline.Length; i++)
+        {
+            discipline[i].Print();
+        }
+
+    }
+}
---
+++ 7 лаба3уровень- 3 номер 2.0.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+abstract class Team
+{
+    private int[] places;
+    public Team(int[] places)
+    {
+        this.places = places;
+    }
+    public int[] Places
+    {
+        get { return places; }
+    }
+    public abstract string Gender { get; }
+    public int CalculatePoints()
+    {
+        int totalPoints = 0;
+        foreach (int place in places)
+        {
+            switch (place)
+            {
+                case 1:
+                    totalPoints += 5;
+                    break;
+                case 2:
+                    totalPoints += 4;
+                    break;
+                case 3:
+                    totalPoints += 3;
+                    break;
+                case 4:
+                    totalPoints += 2;
+                    break;
+                case 5:
+                    totalPoints += 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return totalPoints;
+    }
+}
+
+class WomenTeam : Team
+{
+    public WomenTeam(int[] places) : base(places) { }
+    public override string Gender => "Женская команда";
+}
+
+class MenTeam : Team
+{
+    public MenTeam(int[] places) : base(places) { }
+    public override string Gender => "Мужская команда";
+}
+
+class Program
+{
+    static void Main()
+    {
+
+        Team[] womenteam = new WomenTeam[]
+        {
+            new WomenTeam(new int[] {27,37,36,26,28,29}),
+            new WomenTeam(new int[] {5, 8, 9, 10, 17, 12}),
+            new WomenTeam(new int[] {13, 15, 1, 16, 11, 18})
+        };
+        Team[] menteam = new MenTeam[]
+        {
+            new MenTeam(new int[] {7, 2, 3, 14, 6, 4}),
+            new MenTeam( new int [] {19,23,25,20,30,35}),
+            new MenTeam(new int[] {21,24,33,34,31,22})
+        };
+
+        int winnerIndex = 0;
+        int maxPoints = 0;
+        string winnerGender = "";
+        for (int i = 0; i < womenteam.Length; i++)
+        {
+            int points = womenteam[i].CalculatePoints();
+            Console.WriteLine($"{womenteam[i].Gender} {i + 1}: набрала {points} баллов");
+            if (points > maxPoints)
+            {
+                maxPoints = points;
+                winnerIndex = i;
+                winnerGender = womenteam[i].Gender;
+            }
+        }
+
+        for (int i = 0; i < menteam.Length; i++)
+        {
+            int points = menteam[i].CalculatePoints();
+            Console.WriteLine($"{menteam[i].Gender} {i + 1}: набрала {points} баллов");
+            if (points > maxPoints)
+            {
+                maxPoints = points;
+                winnerIndex = i + womenteam.Length;
+                winnerGender = menteam[i].Gender;
+            }
+        }
+        if(winnerGender == "Женская команда")
+        {
+            Console.WriteLine($"Команда победитель: {winnerGender} ,{winnerIndex + 1} набравшая {maxPoints} баллов.");
+        }
+        else
+        {
+            Console.WriteLine($"Команда победитель: {winnerGender} ,{winnerIndex + womenteam.Length +1} набравшая {maxPoints} баллов.");
+        }
+ 
+    }
+}
