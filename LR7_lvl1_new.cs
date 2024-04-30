8ff83d9756a7b49eb1294f194f653d9c91f7ea11
Valeriya Spiricheva <159257836+Eavslera@users.noreply.github.com>
2024-04-30 22:22:05 +0300

    Add files via upload
    лабораторная работа 7, исправленные номера

---
+++ LR7_lvl1_new.cs
@@ -0,0 +1,101 @@
+﻿
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        abstract class SportData
+        {
+            protected string Name { get; }
+            public double Time { get; }
+
+            protected int Distance;
+
+            protected SportData(string name, double time, int distance)
+            {
+                Name = name;
+                Time = time;
+                Distance = distance;
+            }
+
+            public void Show()
+            {
+                Console.WriteLine("{0, 12}: {1:f1} / Ср. скорость: {2:f1} м/c", Name, Time, Distance / Time);
+            }
+
+            public static void Sort(SportData[] array)
+            {
+                int n = array.Length;
+                int i, j;
+                for (i = 1; i < n; ++i)
+                {
+                    SportData key = array[i];
+                    j = i - 1;
+
+                    while (j >= 0 && array[j].Time > key.Time)
+                    {
+                        array[j + 1] = array[j];
+                        j = j - 1;
+                    }
+                    array[j + 1] = key;
+                }
+            }
+        }
+
+        class RunData100 : SportData
+        {
+            public RunData100(string name, double time) : base(name, time, 100) { }
+        }
+
+        class RunData500 : SportData
+        {
+            public RunData500(string name, double time) : base(name, time, 500) { }
+        }
+
+        static void Main(string[] args)
+        {
+            const int N = 10;
+            string[] surnames = {
+                "Ivanova",
+                "Petrova",
+                "Rudenko",
+                "Klochay",
+                "Vasnecova",
+                "Kan",
+                "Romanova",
+                "Smolina",
+                "Darmograi",
+            };
+            Random rand = new Random();
+            RunData100[] rd100 = new RunData100[N];
+            RunData500[] rd500 = new RunData500[N];
+            for (int i = 0; i < N; i++)
+            {
+                string surname = surnames[rand.Next(surnames.Length - 1)];
+                double time = rand.NextDouble() * 10 + 12;
+                rd100[i] = new RunData100(surname, time);
+            }
+            for (int i = 0; i < N; i++)
+            {
+                string surname = surnames[rand.Next(surnames.Length - 1)];
+                double time = rand.NextDouble() * 10 + 70;
+                rd500[i] = new RunData500(surname, time);
+            }
+
+            SportData.Sort(rd100);
+            SportData.Sort(rd500);
+
+            Console.WriteLine("Бег 100м.");
+            foreach (SportData item in rd100)
+            {
+                item.Show();
+            }
+            Console.WriteLine("Бег 500м.");
+            foreach (SportData item in rd500)
+            {
+                item.Show();
+            }
+        }
+    }
+}
---
+++ LR7_lvl2_new.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        abstract class SportHuman
+        {
+            public string Name { get; }
+            public int Team { get; }
+            public double Time { get; }
+
+            protected SportHuman(string name, double time, int team)
+            {
+                Name = name;
+                Team = team;
+                Time = time;
+            }
+
+            public void Show()
+            {
+                Console.WriteLine("{0}: {1, 10} - {2:f1} секунд", Team, Name, Time);
+            }
+        }
+
+        class SportMan : SportHuman
+        {
+            public SportMan(string name, double time) : base(name, time, 0) { }
+        }
+
+        class SportWoman : SportHuman
+        {
+            public SportWoman(string name, double time) : base(name, time, 1) { }
+        }
+
+        static void Main(string[] args)
+        {
+            const int N = 5;
+            string[] surnames_f = {
+                "Ivanova",
+                "Petrova",
+                "Rudenko",
+                "Klochay",
+                "Vasnecova",
+                "Kan",
+                "Romanova",
+                "Smolina",
+                "Darmograi",
+            };
+
+            string[] surnames_m = {
+                "Ivanov",
+                "Petrov",
+                "Rudenko",
+                "Klochay",
+                "Vasnecov",
+                "Kan",
+                "Romanov",
+                "Smolin",
+                "Kuchuk",
+            };
+            Random rand = new Random();
+            SportMan[] man = new SportMan[N];
+            SportWoman[] woman = new SportWoman[N];
+            SportHuman[] common = new SportHuman[N * 2];
+            for (int i = 0; i < N; i++)
+            {
+                string surname = surnames_f[rand.Next(surnames_f.Length - 1)];
+                double time = rand.NextDouble() * 45 + 60;
+                woman[i] = new SportWoman(surname, time);
+                common[i] = woman[i];
+            }
+            for (int i = 0; i < N; i++)
+            {
+                string surname = surnames_m[rand.Next(surnames_m.Length - 1)];
+                double time = rand.NextDouble() * 60 + 50;
+                man[i] = new SportMan(surname, time);
+                common[i + N] = man[i];
+            }
+
+            Sort_Human(common, N * 2);
+
+            Console.WriteLine("-- Мужчины --");
+            foreach (SportHuman item in man)
+            {
+                item.Show();
+            }
+            Console.WriteLine("-- Женщины --");
+            foreach (SportHuman item in woman)
+            {
+                item.Show();
+            }
+            Console.WriteLine("-- Общее --");
+            foreach (SportHuman item in common)
+            {
+                item.Show();
+            }
+        }
+
+        static void Sort_Human(SportHuman[] humans, int n)
+        {
+            int i = 0;
+            while (i < n)
+            {
+                if (i == 0)
+                    i++;
+                if (humans[i].Time >= humans[i - 1].Time)
+                    i++;
+                else
+                {
+                    SportHuman temp = humans[i];
+                    humans[i] = humans[i - 1];
+                    humans[i - 1] = temp;
+                    i--;
+                }
+            }
+        }
+    }
+}
