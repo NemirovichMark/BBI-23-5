d7c29d98123abcb7ef04d491b388d522ae480fc0
Игорь <159259313+Yavorskiy-Igor-BBI-23-5@users.noreply.github.com>
2024-05-28 21:20:46 +0300

    Lab 10 (C_7)

---
+++ 10/GameCatalog_3.cs
@@ -0,0 +1,20 @@
+﻿public partial class GameCatalog
+{
+    public void SortCatalog()
+    {
+        for (int i = 0; i < games.Count; i++)
+        {
+            var val = games[i];
+            for (int j = i - 1; j >= 0;)
+            {
+                if (val < games[j])
+                {
+                    games[j + 1] = games[j];
+                    j--;
+                    games[j + 1] = val;
+                }
+                else break;
+            }
+        }
+    }
+}
